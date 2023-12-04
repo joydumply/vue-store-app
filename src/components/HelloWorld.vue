e9db0e53f9d977eaa886b15827e4b752c95cf0de
joydumply <nick.babichev@gmail.com>
2023-12-04 18:17:03 +0200

    added actions & mutations

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,9 +10,19 @@ import { useStore, mapGetters } from 'vuex';
 
 const store = useStore();
 const usersList = computed(() => store.getters['usersStore/usersList']);
+
+const addUser = () => {
+	const newUser = {
+		name: 'Ivan',
+		age: 25,
+	};
+	store.dispatch('usersStore/addNewUser', newUser);
+};
 </script>
 
 <template>
 	<div>Helllo</div>
 	{{ usersList }}
+
+	<button @click="addUser">Add user</button>
 </template>
--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -11,8 +11,18 @@ const usersStore = {
 	getters: {
 		usersList: (state) => Object.values(state.list),
 	},
-	mutations: {},
-	actions: {},
+	mutations: {
+		ADD_USER(state, user) {
+			state.list[user.id] = user; // работает на Vue 3 но не Vue 2
+			//Vue.set(state.list, user.id, user); /*  работает на Vue 2 / не работает на Vue 3 */
+		},
+	},
+	actions: {
+		addNewUser({ commit }, user) {
+			const newUser = { ...user, id: String(Math.random()) };
+			commit('ADD_USER', newUser);
+		},
+	},
 };
 
 export default usersStore;
